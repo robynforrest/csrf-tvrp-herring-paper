96fa1e8570ecf4a58f0bab4f5b0aea417d51c5f9
robynforrest <robynforrest@gmail.com>
2026-01-02 12:18:25 -0800

    Setting up 0-run-analyses.R

--- R/0_run-analyses.R
+++ R/0_run-analyses.R
@@ -1,11 +1,78 @@
-use
+# ======================================================================================================
+# ======== 0 Run all the analyses from here ================================================
+# Load packages, universal settings, source code
+
+# CSRF Project for paper on time-varying reference points for Pacific herring
+
+# AUTHORS/CONTRIBUTORS:
+# Robyn Forrest (RF) (DFO)
+# Quang Huynh (QH) (Blue Matter Science)
+# Adrian Hordyk (AH) (Blue Matter Science)
+
+# Created January 2 2026, following earlier exploratory repository (2023)
+
+library(here)
+
+omspecies <- "pac-herring"
+
+# DO NOT CHANGE (the order is important wrt the iscam files)
+allpacherringstocks <- c("HG","SoG","WCVI") #don't change this, need it so that OM picks the right data folder
+pacherringstocks <- allpacherringstocks
+
+# MAKE DIRECTORIES
+SpDirOM <- here("OMs")
+if(!file.exists(SpDirOM)) dir.create(SpDirOM, recursive=TRUE)
+SpDirMSE <- here("MSEs")
+if(!file.exists(SpDirMSE)) dir.create(SpDirMSE, recursive=TRUE)
+SpDirFigs <- here("Figures")
+if(!file.exists(SpDirFigs)) dir.create(SpDirFigs, recursive=TRUE)
+
+# Universal settings that are changed frequently
+nsim <- 100
+pro_years <- 50
+Mage <- 3 # age at which to extract constant M from cpars M_age_array
+
+# OM SETTINGS
+# Random walk parameters for om M
+alpha_m_inc <- 0.013 # setting for random walk in M with upward drift
+sigma_m   <- 0.03 # setting for random walk in M
+
+# what sd for M (across reps) should we base M projections on? nyears-sd_numyears.
+# sd across reps for M is really large in final year, sample from sd_numyears earlier
+sd_numyears <- 3
+
+# Smoothing parameters for future M
+msplines <- TRUE # Choose whether to smooth projected M random walks with spline
+knotdiv <- 3 # How many years to divide proyears by for splines (smaller number means more knots, e.g., 30/3 = 10 knots)
+
+fix_om_waa_sel <- FALSE # Set whether to replace waa and sel in historical OM as constant values
+
+# MP SETTINGS
+# Settings for assessment model and plots (not used in this version of the repo)
+# Interval <- 1 #1 # stock assessment interval
+# diagnosticMP <- "full" # Setting for make_MP. Can be "none"  "min" or "full". See ??make_MP
+# plot_retros <- FALSE #TRUE # set whether to plot retrospectives ... takes a while
+# plot_M_timeseries <- TRUE # set whether to plot time series of estimated M - a bit slow
+
+# ======================================================================================================
+# Choose whether to run the OMs and MSEs (only need to do this once)
+make_oms <- T #T/F Turn off if you have already made and saved the base OMs
+run_mses <- T #T/F Turn off if you have already run the MSEs.
+plot_mses <- T #T/F
+# ======================================================================================================
+
+# Source files to load packages and set universal settings
+# This will also source the file that makes the MPs
+# Also sources the files that make all the functions to extract MSE objects
+source("R/0_settings.R")
+
 # Build OMs
 # Looping through species is handled in 1_make-oms.R
 # If make_oms == F, this won't do anything
 # Makes plots as well
 source("R/1a_make-pac-herring-oms.R")
 
-# Run MSEs
+# Run MSEs with no fishing
 # If run_mses == F, this won't do anything
 # Makes plots as well
 source("R/2a_run-pac-herring-mses.R")
@@ -13,44 +80,3 @@ source("R/2a_run-pac-herring-mses.R")
 # ====================================================================================================================
 # ======= END OF SCRIPT ==============================================================================================
 # ====================================================================================================================
-
-# FINDING THINGS
-# hOMs[[i]] is the parameter inputs for the OM
-# Use slotNames to query the slots of the OM (OM is S4 class)
-# Use the @ operator rather than the $
-# e.g.:
-# slotNames(hOMs[[i]])  to see the slots
-# hOMs[[i]]@R0  to see the value
-# cpars has a lot of extra stuff in it including time series of anomalies (Perr_y)
-# hOMs[[i]]@cpars
-
-# To see the data object being produced by the OM:
-# hist_hMSEs[[1]]@Data
-
-# Some this might be out of date given the new SAMtool
-# For the MSE runs. Each MSE rds file has the four M scenarios in it.
-# Query the same as for the OM file:
-# slotNames(MSEfile[[i]]) to see the slots
-# If an extended analysis was run. Use mse@Misc$extended to see the estimated
-#   values across sims for:
-# N, B, SSB, Catch, Removals, FM, FMret
-
-# Find assessment outputs - only if the MP diagnostic is set to "full"
-# mse@PPD contains all the "rep files" from each simulation including Mort,
-# but this is a point estimate
-# M is buried deep!! k = mp, sim=simulation,  y = assessment year,
-# Choose the first column as it is the same for all ages
-# MSE@PPD[[k]]@Misc[[sim]]$Assessment_report[[y]]@TMB_report$M[,1]
-# SO, for each MP in each assessment year we want the spread of M across simulations
-
-# Annual estimated Ref points are reported in Hist@Ref$ByYear and MSE@RefPoint$ByYear
-# The averaged MSY reference points are returned in MSE@RefPoint
-
-# Note from openMSE wiki (https://openmse.com/tutorial-reference-points/msy_ref_points/):
-# The MSY metrics in the MSE object are always calculated using annual values.
-# The slot MSE@SB_SBMSY returns the spawning biomass in the projections divided by SBMSY
-# in each year of the projections.
-# For alternative methods to calculate SB/SBMSY, such as relative to the constant SBMSY described above,
-# use MSE@SSB or MSE@B and the data stored in MSE@RefPoint.
-
-
--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # csrf-tvrp-herring-paper
 
-This repository contains the code for the paper "INSERT NAME HERE" exploring the use of time-varying equilibrium reference points for three Pacific Herring stocks.
+This repository contains the code for the paper "INSERT NAME HERE" exploring the 
+use of time-varying equilibrium reference points for three Pacific Herring stocks.
 
 Run everything from 0_run-analyses.R.
 
